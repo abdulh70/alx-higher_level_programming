39b9d902c2fa714bc80230d5c55ea969cf528258
abdulh70 <abdulazizh70@gmail.com>
2023-03-08 16:51:26 -0800

    added file

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,8 +1,5 @@
-
 #!/usr/bin/python3
-# 2-print_alphabet.py
-
-"""Print the alphabet in lowercase"""
-for letter in range(97, 123):
-    print("{}".format(chr(letter)), end="")
-
+for i in range(97, 123):
+    tableAsciiToAlphabet = chr(i)
+    asciiToAlpha = "" + tableAsciiToAlphabet
+    print("{}".format(asciiToAlpha), end='')
